29dcc64a08fc7085b6f3c87b48c97f97f9d584f7
Rohan Parulekar <rohan.s.parulekar@nuagenetworks.net>
2017-05-16 13:53:39 -0700

    Fix for RPM build script

--- scripts/buildRPM.sh
+++ scripts/buildRPM.sh
@@ -18,18 +18,18 @@ rm -rf ~/rpmbuild/RPMS/x86_64/nuage*
 rm -rf ~/rpmbuild/SRPMS/nuage*
 rm -rf /tmp/nuage-cni*
 
-cd $GOPATH/src/nuage-mesos-cni
+cd $GOPATH/src/nuage-cni
 go build -o nuage-cni-mesos nuage-cni.go
 go build -o nuage-cni-k8s nuage-cni.go
 go build -o nuage-cni-openshift nuage-cni.go
 
 cd /tmp
-cp -r $GOPATH/src/nuage-mesos-cni nuage-cni-mesos-${version}
-cp -r $GOPATH/src/nuage-mesos-cni nuage-cni-k8s-${version}
-cp -r $GOPATH/src/nuage-mesos-cni nuage-cni-openshift-${version}
+cp -r $GOPATH/src/nuage-cni nuage-cni-mesos-${version}
+cp -r $GOPATH/src/nuage-cni nuage-cni-k8s-${version}
+cp -r $GOPATH/src/nuage-cni nuage-cni-openshift-${version}
 tar -czvf $HOME/rpmbuild/SOURCES/nuage-cni-mesos-${version}.tar.gz nuage-cni-mesos-${version}
 tar -czvf $HOME/rpmbuild/SOURCES/nuage-cni-k8s-${version}.tar.gz nuage-cni-k8s-${version}
 tar -czvf $HOME/rpmbuild/SOURCES/nuage-cni-openshift-${version}.tar.gz nuage-cni-openshift-${version}
-rpmbuild -ba $GOPATH/src/nuage-mesos-cni/rpmbuild/nuage-cni-mesos.spec
-rpmbuild -ba $GOPATH/src/nuage-mesos-cni/rpmbuild/nuage-cni-k8s.spec
-rpmbuild -ba $GOPATH/src/nuage-mesos-cni/rpmbuild/nuage-cni-openshift.spec
+rpmbuild -ba $GOPATH/src/nuage-cni/rpmbuild/nuage-cni-mesos.spec
+rpmbuild -ba $GOPATH/src/nuage-cni/rpmbuild/nuage-cni-k8s.spec
+rpmbuild -ba $GOPATH/src/nuage-cni/rpmbuild/nuage-cni-openshift.spec
